bf420254981be61e302b9e661309c41984bb240c
Mikhail Malykhin <malykhinmikhail@rambler.ru>
2020-08-05 23:01:54 +0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { SelectorComponent } from './components/selector/selector.component';
+import { OrganizerComponent } from './components/organizer/organizer.component';
+import {MomentPipe} from "./shared/moment.pipe";
+
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from "@angular/material/table";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganizerComponent,
+    MomentPipe
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatTableModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
